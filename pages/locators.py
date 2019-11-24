e6da346c00c0df7bc76eec50c0bcd6f42e42992c
AntonRassadin <antonrsdn@gmail.com>
2019-11-25 00:18:07 +0300

    add login page

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,10 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_EMAIL_ADRESS = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "is not a login url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert (self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_ADRESS) 
+                and self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD))
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert (self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL) 
+                and self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD) 
+                and self.is_element_present(*LoginPageLocators.REG_CONFIRM_PASSWORD))
