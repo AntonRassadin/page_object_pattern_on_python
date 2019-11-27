eef9e247902bff8952c8e92741b275c1daa81256
AntonRassadin <antonrsdn@gmail.com>
2019-11-27 15:41:49 +0300

    transfer go_to_login functionality to base_page class and add test 'go to login from product page'

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 import pytest
 
+@pytest.mark.huy
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -21,3 +22,4 @@ def test_pass_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+    
